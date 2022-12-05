2458444784d4efef3fc8752d045c637dc4ef2786
Vinayaka98 <67480456+Vinayaka98@users.noreply.github.com>
2022-12-04 21:52:14 -0500

    Add files via upload

---
+++ 101231430_comp3123_assignment2_reactjs-main/src/components/CreateEmployee.js
@@ -0,0 +1,168 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+import { Button } from 'react-bootstrap';
+import {Link, useNavigate} from 'react-router-dom';
+
+import {toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+import { useForm } from "react-hook-form";
+
+export default function  CreateEmployee() {
+
+    const {register,formState: { errors },handleSubmit} = useForm();
+
+    const notifySuccess = () => toast("Employee Created Successfully!")
+    
+    const notifyError = () => toast.warn('Please check again the email!', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
+    
+    const notifyErrorFirstName = () => toast.error('First Name cannot be empty', {
+        position: "top-center",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        });;
+
+        const notifyErrorLastName = () => toast.error('Last Name cannot be empty', {
+            position: "top-center",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+            });;
+            
+        const notifyErrorEmail = () => toast.error('Email cannot be empty', {
+            position: "top-center",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+            });;
+    
+        const notifyErrorEmailFormat = () => toast.error('Email format is incorrect', {
+            position: "top-center",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+            })
+    
+    const navigate = useNavigate();
+    const base_url = 'https://comp3123-assignment1-nodejs.herokuapp.com/api/emp/employees';
+    
+
+    const [employee, setEmployee] = useState({
+        first_name: '',
+        last_name: '',
+        email_id: '',
+        employee:[]
+
+    });
+
+    const handleInputChange = (e) => {
+        const {name, value} = e.target;
+
+        setEmployee({...employee, [name]: value})
+    }
+    
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        const newEmployee = {
+            first_name: employee.first_name,
+            last_name: employee.last_name,
+            email_id: employee.email_id
+        }
+
+        if (newEmployee.first_name.length <= 0 || newEmployee.last_name.length <= 0 || newEmployee.email_id.length <= 0) {
+            if (newEmployee.first_name.length <= 0) {
+                notifyErrorFirstName();
+            }
+            if (newEmployee.last_name.length <= 0) {
+                notifyErrorLastName();
+            }
+            if (newEmployee.email_id.length <= 0) {
+                notifyErrorEmail();
+            }
+        } else if (!newEmployee.email_id.includes('@')) {
+            notifyErrorEmailFormat();
+            } 
+        else {
+            axios.post(base_url, newEmployee)
+            .then(res => {
+                notifySuccess();
+                navigate('/employees');
+            })
+            .catch(err => {
+                notifyError()
+            })
+        }
+    }
+    
+    return (
+            <div>
+                <div style={{marginTop:"20px", marginBottom:"20px"}} className="row">
+                    <div className="card col-md-6 offset-md-3 offset-md-3">
+                        <h3 className="text-center">Add Employee</h3>
+                        <div className="card-body">
+
+                            <form onSubmit={e => handleSubmit(onSubmit(e))}>
+                                <div className="form-group d-block">
+                                    <label>First Name: </label>
+                                    <input className="form-control" placeholder=""
+                                    {...register("first_name", { required: true })}
+                                    onChange={e => handleInputChange(e)}/>
+                                    <error>
+                                        {errors.first_name?.type === "required" && "Name is required"}
+                                    </error> 
+                                </div>
+
+                                <div className="form-group">
+                                    <label>Last Name: </label>
+                                    <input  className="form-control" placeholder=""
+                                    {...register("last_name", { required: true })}
+                                    onChange={e => handleInputChange(e)}/>
+                                </div>
+
+                                <div className="form-group">
+                                    <label>Email Id: </label>
+                                    <input className="form-control" placeholder=""
+                                    {...register("email_id", { required: true })}
+                                    onChange={e => handleInputChange(e)}/>
+                                </div>
+
+                                <Button variant="primary" onSubmit={handleSubmit(onSubmit)} type='submit'>Submit</Button>
+                                    &nbsp;&nbsp;&nbsp;
+                                <Link to={"/employees"}><Button className="btn btn-danger" >Cancel</Button>
+                                </Link>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <ToastContainer />
+            </div>
+            
+        );
+}
---
+++ 101231430_comp3123_assignment2_reactjs-main/src/components/Navigation.js
@@ -0,0 +1,66 @@
+import {Navbar, Nav} from 'react-bootstrap';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import CreateEmployee from "./CreateEmployee";
+import EmployeeList from "./EmployeeList";
+import Home from "./Home";
+import UpdateEmploye from "./UpdateEmployee";
+import ViewEmployee from "./ViewEmployee";
+import SignUp from "./SignUp";
+import LogIn from "./LogIn";
+import React, {useState} from "react";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function Navigation() {
+
+    const[setNavigate] = useState(false);
+    const Logout = () => {
+
+        localStorage.removeItem('login');
+        setNavigate(true);
+    }
+   
+return (
+    
+    <Router>
+        <div>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand>Employee Management System</Navbar.Brand>
+                <div style={{padding:"10px", fontFamily:"inherit",fontSize:"20px"}}>
+                <Nav className="mr-auto">
+                    <Nav.Link className="btn btn-secondary" style={{color:"blanchedalmond"}} href="/">Home</Nav.Link>
+                    <Nav.Link className="btn btn-info" style={{marginLeft:"40px", color:"blue"}} href="/employees">Employees</Nav.Link>
+                </Nav>
+                </div>
+                <Link to={"/"} onClick={Logout} className="btn btn-outline-danger w-25" style={{marginLeft:"180px", color:"white"}}>Logout</Link>
+            </Navbar>
+            <Routes>
+                <Route path={"/"} element={<Home/>}/>
+                <Route path={"/signup"} element={<SignUp/>}/>
+                <Route path={"/login"} element={<LogIn/>}/>
+                <Route path={"/employees"} element={<EmployeeList/>}/>
+                <Route path={"add-employees"} element={<CreateEmployee/>}/>
+                <Route path={"employees/update-employees/:id"} element={<UpdateEmploye/>}/>
+                <Route path={"employees/view-employees/:id"} element={<ViewEmployee/>}/>
+            </Routes>
+        </div>
+        <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        />
+    </Router>
+    
+    );
+}
+
+export default Navigation;
+
+            
